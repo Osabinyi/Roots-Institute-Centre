f8d151e0ecd9a785f4192599c98f4d99e4a3ffc7
Osabinyi <47927307+Osabinyi@users.noreply.github.com>
2020-05-03 11:55:34 +0300

    Add files via upload

---
+++ mum.js
@@ -0,0 +1,95 @@
+// const filters = function (filter) {
+//    filter.section = 'sign'
+// }
+
+// const section = document.getElementById('sign')
+// const nav = document.getElementById('nav-bar')
+// const div = document.getElementById('carousel-example-2')
+
+// if (filters === nav) {
+//    console.log('please provide a name')
+// }else {
+//    console.log(filters)
+// }
+
+
+
+
+
+
+//   const filter = [] 
+  
+//   const searchBar = document.getElementById('myInput');
+//    searchBar.addEventListener('keyup', e => {
+//     const searchString = e.target.value.toLowerCase();
+//     const filteredCharacters = hpCharacters.filter(character => {
+//       return (
+//         character.name.toLowerCase().includes(searchString) || character.house.toLowerCase().includes(searchString));
+//     });
+//     displayCharacters(filteredCharacters);
+// });
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   //search another style
+           // filter books
+
+        // const searchBar = document.body['myInput'].queryselector('input');
+
+        // searchBar.addEventListener('keyup', function(e){
+
+        //     const term = e.target.value.toLowerCase();
+
+        //     const books = list.getElementsByTagName('filter');
+
+        //     Array.from(books).forEach(function(book){
+
+        //         const title = book.firstElementChild.text.Content;
+
+        //         if (title.toLowerCase().indexOf(term) != -1) {
+
+        //             book.style.disply = 'block';
+
+        //         } else {
+
+        //             book.style.disply = 'none';
+
+        //         }
+
+        //     })
+
+        // })
+
+        //end and another styleusing includes
+//<form id="search-truck">
+    //<input type="text">
+ // </form>
+  //  <ul class="menubar" id="menuList" style="list-style: none;"></ul>
+
+
+//var list = document.querySelector('#menuList');
+
+
+//const searchBar = document.forms['search-truck'].querySelector('input');
+//searchBar.addEventListener('keyup', function(e){
+//  const term = e.target.value.toLowerCase();
+//  const books = list.getElementsByTagName('li');
+//  Array.from(books).forEach(function(book){
+//  console.log("book: "+ book.textContent);
+//    const title = book.textContent.toLowerCase();
+//    if(title.includes(term)){ 
+//      book.style.display = 'block';
+//    }
+ //   else { 
+ //     book.style.display = 'none';
+ //   } 
+  //})
+//})
